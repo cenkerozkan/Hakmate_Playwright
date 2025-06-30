57194d37946bb3d79e26dec8295cec74d4fbd2e6
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-07-01 00:51:13 +0300

    Better naming for timeout decorator

--- decorators/with_timeout.py
+++ decorators/with_timeout.py
@@ -8,29 +8,27 @@ from playwright.sync_api import Page
 
 from exceptions.not_web_testable_exception import NotWebTestableException
 
-def timeout_decorator(func: Callable) -> Callable:
+def with_timeout(func: Callable) -> Callable:
     sig: inspect.Signature = inspect.signature(func)
 
     if "page" not in sig.parameters or not issubclass(sig.parameters["page"].annotation, Page):
-        raise NotWebTestableException("Decorated method can only take testable Page objects!")
+        raise NotWebTestableException()
 
     @wraps(func)
     def wrapper(*args: typing.Any, **kwargs: typing.Any) -> typing.Callable:
-        page_arg: Page = None
+        page_arg: Page | None = None
 
         bound_args = sig.bind(**kwargs)
         if "page" in bound_args.arguments:
             page_arg = bound_args.arguments["page"]
 
         result = func(*args, **kwargs)
-        page_arg.wait_for_timeout(3000)
+        page_arg.wait_for_timeout(5000)
         return result
     return wrapper
 
 
 if __name__ == "__main__":
-    print("Hello World")
-
-    @timeout_decorator
+    @with_timeout
     def test(page: Page):
         pass
