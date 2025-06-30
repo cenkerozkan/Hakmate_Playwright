8c41820cad89254d3c2398a1c2a821a26feb5f34
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-07-01 00:52:32 +0300

    Minor refactoring on not_web_testable_exception.py by adding a default message value.

--- exceptions/not_web_testable_exception.py
+++ exceptions/not_web_testable_exception.py
@@ -1,6 +1,9 @@
 from typing import Text
 
 class NotWebTestableException(Exception):
-    def __init__(self, message: Text) -> None:
+    def __init__(
+            self,
+            message: Text = "Decorated method can only take testable Page objects!"
+    ) -> None:
         self.message = message
         super().__init__(self.message)
